794c4070784fac93eaa341d1d1f463eff008ed43
jonathan duty <jonathanduty2021@u.northwestern.edu>
2021-06-01 16:00:56 -0500

    commiting

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+if (process.env.MONGODB_URI) {
+  console.log(process.env.MONGODB_URI)
+}
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
+  useNewUrlParser: true,
+  useFindAndModify: false
+  }
+);
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
